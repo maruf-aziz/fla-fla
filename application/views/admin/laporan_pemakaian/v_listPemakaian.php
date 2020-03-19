dd967ac4f9d70c533be5c8fa8390c1bd0bf615cd
M Taftazani Adi <m.taftazani123@gmail.com>
2020-03-19 13:30:10 +0700

    Update Table - Stock Extra

--- application/views/admin/laporan_pemakaian/v_listPemakaian.php
+++ application/views/admin/laporan_pemakaian/v_listPemakaian.php
@@ -1,6 +1,5 @@
 <script>
-	function printDiv(divName)
-	{
+	function printDiv(divName) {
 		var printContent = document.getElementById(divName).innerHTML;
 		var originalContent = document.body.innerHTML;
 
@@ -14,7 +13,7 @@
 
 <div class="page-title">
 	<div class="title_left">
-		<h3>Laporan Pemakaian</h3>		 
+		<h3>Laporan Pemakaian</h3>
 	</div>
 
 	<div class="title_right">
@@ -33,8 +32,8 @@
 
 <div class="row">
 	<div class="col-md-12 col-sm-12 col-xs-12">
-	<button onclick="printDiv('content')" class="btn btn-primary"><i class="fa fa-print"> &nbsp;<b>Print</b></i></button>
-	<button onclick="window.history.back()" class="btn btn-danger"><i class="fa fa-reply"> &nbsp;<b>Back</b></i></button>
+		<button onclick="printDiv('content')" class="btn btn-primary"><i class="fa fa-print"> &nbsp;<b>Print</b></i></button>
+		<button onclick="window.history.back()" class="btn btn-danger"><i class="fa fa-reply"> &nbsp;<b>Back</b></i></button>
 
 		<div class="x_panel">
 			<div class="x_title">
@@ -48,9 +47,9 @@
 				<div class="clearfix"></div>
 			</div>
 			<div class="x_content" id="content">
-			<h4 align="center">
-				Region : <a class="btn btn-primary btn-sm"><?=$region?></a> | Tanggal : <a class="btn btn-primary btn-sm"><?=$tanggal?></a> | Shift : <a class="btn btn-primary btn-sm"><?=$shift?></a>
-			</h4>
+				<h4 align="center">
+					Region : <a class="btn btn-primary btn-sm"><?= $region ?></a> | Tanggal : <a class="btn btn-primary btn-sm"><?= $tanggal ?></a> | Shift : <a class="btn btn-primary btn-sm"><?= $shift ?></a>
+				</h4>
 				<div class="row">
 					<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
 						<div class="table-responsive">
@@ -112,8 +111,7 @@
 									foreach ($topping as $key => $value) {
 										if ($value->harga_jual != 0) {
 											$harga = $value->harga_jual;
-										}
-										else{
+										} else {
 											$harga = $value->harga;
 										}
 									?>
@@ -218,27 +216,27 @@
 
 								<tbody>
 									<?php
-                  $bs = 0;
-                  $pm = 0;
+									$bs = 0;
+									$pm = 0;
 									$total_bs = 0;
 									$total_pm = 0;
 									foreach ($susu_putih as $key => $value) {
 
-                    $bs = $value->basic_b + $value->basic_p + $value->basic_y;
-                    $pm = $value->pm_b + $value->pm_p;
+										$bs = $value->basic_b + $value->basic_p + $value->basic_y;
+										$pm = $value->pm_b + $value->pm_p;
 
-                    $total_bs = $total_bs + $bs;
-                    $total_pm = $total_pm + $pm;
+										$total_bs = $total_bs + $bs;
+										$total_pm = $total_pm + $pm;
 
 									?>
 
 										<tr>
-											<td><?=($value->id_jenis != 3 && $value->id_jenis != 4 && $value->id_jenis != 6 ? $value->nama_jenis : '') ?></td>
-											<td><?=($value->id_jenis != 3 && $value->id_jenis != 4 && $value->id_jenis != 6 ? $bs : '') ?></td>
-											<td><?=($value->id_jenis != 3 && $value->id_jenis != 4 && $value->id_jenis != 6 ? $pm : '') ?></td>
+											<td><?= ($value->id_jenis != 3 && $value->id_jenis != 4 && $value->id_jenis != 6 ? $value->nama_jenis : '') ?></td>
+											<td><?= ($value->id_jenis != 3 && $value->id_jenis != 4 && $value->id_jenis != 6 ? $bs : '') ?></td>
+											<td><?= ($value->id_jenis != 3 && $value->id_jenis != 4 && $value->id_jenis != 6 ? $pm : '') ?></td>
 										</tr>
 
-                    <?php
+									<?php
 
 									}
 
@@ -267,28 +265,28 @@
 								</thead>
 
 								<tbody>
-                <?php
-                  $bs = 0;
-                  $pm = 0;
+									<?php
+									$bs = 0;
+									$pm = 0;
 									$total_bs = 0;
 									$total_pm = 0;
 									foreach ($susu_coklat as $key => $value) {
 
-                    $bs = $value->basic_b + $value->basic_p;
-                    $pm = $value->pm_b + $value->pm_p;
+										$bs = $value->basic_b + $value->basic_p;
+										$pm = $value->pm_b + $value->pm_p;
 
-                    $total_bs = $total_bs + $bs;
-                    $total_pm = $total_pm + $pm;
+										$total_bs = $total_bs + $bs;
+										$total_pm = $total_pm + $pm;
 
 									?>
 
 										<tr>
-											<td><?=($value->id_jenis != 1 && $value->id_jenis != 2 && $value->id_jenis != 5 ? $value->nama_jenis : '') ?></td>
-											<td><?=($value->id_jenis != 1 && $value->id_jenis != 2 && $value->id_jenis != 5 ? $bs : '') ?></td>
-											<td><?=($value->id_jenis != 1 && $value->id_jenis != 2 && $value->id_jenis != 5 ? $pm : '') ?></td>
+											<td><?= ($value->id_jenis != 1 && $value->id_jenis != 2 && $value->id_jenis != 5 ? $value->nama_jenis : '') ?></td>
+											<td><?= ($value->id_jenis != 1 && $value->id_jenis != 2 && $value->id_jenis != 5 ? $bs : '') ?></td>
+											<td><?= ($value->id_jenis != 1 && $value->id_jenis != 2 && $value->id_jenis != 5 ? $pm : '') ?></td>
 										</tr>
 
-                    <?php
+									<?php
 
 									}
 
@@ -312,11 +310,15 @@
 							<table class="table table-striped jambo_table bulk_action" id="myTable1">
 								<thead>
 									<tr class="headings">
-										<th class="column-title">Nama Ekstra </th>
-										<th class="column-title">Stok Awal </th>
-										<th class="column-title">Penambahan </th>
-										<th class="column-title">Pemakaian</th>
-										<th class="column-title">Sisa</th>
+										<th class="column-title" rowspan="2" style="text-align: center; vertical-align: middle">Nama Ekstra </th>
+										<th class="column-title" rowspan="2" style="text-align: center; vertical-align: middle">Stok Awal </th>
+										<th class="column-title" rowspan="2" style="text-align: center; vertical-align: middle">Penambahan </th>
+										<th class="column-title" colspan="2" style="text-align: center">Pemakaian</th>
+										<th class="column-title" rowspan="2" style="text-align: center; vertical-align: middle">Sisa</th>
+									</tr>
+									<tr>
+										<th class="column-title" style="text-align: center">Hari ini</th>
+										<th class="column-title" style="text-align: center">Bulan ini</th>
 									</tr>
 								</thead>
 
@@ -328,21 +330,20 @@
 										$pemakaian = 0;
 										if ($value->nama_ekstra == "Susu Putih" || $value->nama_ekstra == "Susu Coklat") {
 											$pemakaian = $value->pakai_susu;
-										} 
-										else if($value->nama_ekstra == "Hazel" || $value->nama_ekstra == "Rum" || $value->nama_ekstra == "Lychee"){
+										} else if ($value->nama_ekstra == "Hazel" || $value->nama_ekstra == "Rum" || $value->nama_ekstra == "Lychee") {
 											$pemakaian = $value->sirup;
-										}
-										else {
+										} else {
 											$pemakaian = $value->pakai;
 										}
 									?>
 
 										<tr>
 											<td><?= $value->nama_ekstra ?></td>
-											<td><?= $value->stock_awal ?></td>
-											<td><?= $value->penambahan ?></td>
-											<td><?= ($pemakaian != null ? $pemakaian : 0) ?></td>
-											<td><?= $value->sisa ?></td>
+											<td style="text-align: center"><?= $value->stock_awal ?></td>
+											<td style="text-align: center"><?= $value->penambahan ?></td>
+											<td style="text-align: center"><?= ($pemakaian != null ? $pemakaian : 0) ?></td>
+											<td style="text-align: center"></td>
+											<td style="text-align: center"><?= $value->sisa ?></td>
 										</tr>
 
 									<?php
